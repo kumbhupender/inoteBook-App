478b2bfa88fccf56918f6bb0249612c10c235208
Bhupender kumar <kumarbhupendra8171@gmail.com>
2023-07-08 22:17:59 +0530

    Setup create react-app and build backend folder inside this and setup express and mongodb

--- src/App.js
+++ src/App.js
@@ -23,3 +23,15 @@ function App() {
 }
 
 export default App;
+
+
+/*
+  Basically this Inotebook app is based, if user want to make a notes and upload it on the cloud they can do it very easily
+  by using this inotebook . There are lots of features like login , signup and most important part we create our own api
+  by using NodeJs , eXpressJs which is seperate from the frontend part but they both work together , 
+  ************THIS APP IS BASICALLY MADE WITH MERN *******
+
+
+
+
+*/
